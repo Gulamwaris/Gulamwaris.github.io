557c93d7fa67e4ef6fe75af2c9a28290f9b8c12f
Gulamwaris Warsi <gwarsi70@gmail.com>
2019-12-02 12:10:12 +0530

    Heroku Push

---
+++ precache-manifest.ddc2301211e494e89c9744470ef12665.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab304d6224cd140665a5676e1cd9d4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b298049be0036ba4a27",
+    "url": "/static/css/main.90281331.chunk.css"
+  },
+  {
+    "revision": "bcbb2e23b11e70cb0edd",
+    "url": "/static/js/2.25358054.chunk.js"
+  },
+  {
+    "revision": "2b298049be0036ba4a27",
+    "url": "/static/js/main.c618bdc8.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "926281180e2704f167139c48306230cc",
+    "url": "/static/media/avatar-png-1.92628118.png"
+  },
+  {
+    "revision": "99c75a252c58bf0115acb9aea9184a83",
+    "url": "/static/media/like.99c75a25.svg"
+  },
+  {
+    "revision": "79af482615b9e61b7a4ec049057162b7",
+    "url": "/static/media/search_icon_pr2sma.79af4826.svg"
+  },
+  {
+    "revision": "9d1c42a4c783d311d34f2a8395b65491",
+    "url": "/static/media/upload.9d1c42a4.svg"
+  }
+]);
